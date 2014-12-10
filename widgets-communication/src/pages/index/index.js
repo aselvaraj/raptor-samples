34f3afea3465e062bc9a48f93bfd366d7dc33cad
aselvaraj <aselvaraj@ebay.com>
2014-12-09 20:10:48 -0800

    adding an async fragement to the page

--- widgets-communication/src/pages/index/index.js
+++ widgets-communication/src/pages/index/index.js
@@ -1,10 +1,17 @@
 var template = require('marko').load(require.resolve('./template.marko'));
 
 module.exports = function(req, res) {
+    var asyncDataProvider = function asyncDataProvider(args, cb){
+      setTimeout(function (){
+        cb(null, {});
+      }, 1000);
+    };
+
     res.setHeader('Content-Type', 'text/html; charset=utf-8');
     template
         .stream({
-            name: 'John'
+            name: 'John',
+            asyncDP: asyncDataProvider
         })
         .pipe(res);
 };
